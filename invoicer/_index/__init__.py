19ac3ac78344727c4f3d9fcf2edc17156bfe6272
Timothy McFadden <timothy.mcfadden@broadcom.com>
2018-03-22 19:18:18 -0600

    mock up display of annual summary

--- invoicer/_index/__init__.py
+++ invoicer/_index/__init__.py
@@ -1,3 +1,4 @@
+import json
 from flask import Blueprint, render_template, redirect, url_for, session
 
 from ..common import login_required
@@ -27,9 +28,27 @@ def paginate_index(page):
 def index():
     per_page = User.query.get(session['user_id']).profile.index_items_per_page
     invoices = Invoice.query.filter_by(user=User.query.get(session['user_id'])).order_by(Invoice.id.desc()).paginate(page=1, per_page=per_page)
+    date_by_year = {
+        '2018': {
+            'labels': ['Jan', 'Feb', 'Mar', 'Apr'],
+            'series': [
+                [287, 385, 490, 492],
+                [67, 152, 143, 240],
+            ]
+        },
+        '2017': {
+            'labels': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            'series': [
+                [287, 385, 490, 492, 500, 550, 623, 676, 710, 799, 811, 900],
+                [67, 152, 143, 240, 275, 334, 455, 498, 554, 577, 654, 732],
+            ]
+        }
+    }
+
     return render_template(
         'index/lb-index.html',
-        invoices=invoices
+        invoices=invoices,
+        date_by_year=json.dumps(date_by_year)
     )
 
 @index_page.route('/t1')
