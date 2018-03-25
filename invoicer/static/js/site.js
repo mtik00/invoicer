3957c51d682b12ff4cb2b445879879c0fc26bd3f
Timothy McFadden <tim@timandjamie.com>
2018-03-25 11:22:13 -0600

    cleaning up unused template files

--- invoicer/static/js/site.js
+++ invoicer/static/js/site.js
@@ -18,7 +18,6 @@ $().ready(function() {
     }
 
     $('.fixed-plugin a').click(function(event) {
-        // Alex if we click on switch, stop propagation of the event, so the dropdown will not be hide, otherwise we set the  section active
         if ($(this).hasClass('switch-trigger')) {
             if (event.stopPropagation) {
                 event.stopPropagation();
