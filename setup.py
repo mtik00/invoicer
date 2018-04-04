8bf93b46bb46a9a001d15a886f06b180290ac6c6
Timothy McFadden <tim@timandjamie.com>
2018-04-03 18:42:34 -0600

    adding python-memcached Linux requirement

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ setup(
         'premailer==3.1.1',
         'ruamel.yaml==0.15.37',
         'SQLAlchemy-Utils==0.32.21',
-        'uwsgi==2.0.17;platform_system=="Linux"'
+        'uwsgi==2.0.17;platform_system=="Linux"',
+        'python-memcached==1.59;platform_system=="Linux"'
     ],
     extras_require={
         'test': ['pytest-flask==0.10.0', 'flake8'],
