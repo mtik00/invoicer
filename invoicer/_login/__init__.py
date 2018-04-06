e51c55a188ca0539e3fcd77d71e738e995f12089
Timothy McFadden <timothy.mcfadden@broadcom.com>
2018-04-06 10:23:30 -0600

    pep8

--- invoicer/_login/__init__.py
+++ invoicer/_login/__init__.py
@@ -1,6 +1,5 @@
 from flask import (
-    Blueprint, render_template, request, flash, redirect, url_for, session,
-    abort)
+    Blueprint, render_template, request, flash, redirect, url_for, session)
 
 from flask_login import login_user, logout_user, login_required, current_user
 
