617ee1a42588ccb0abdae15071938ad988b15f4b
Tim McFadden <tim@timandjamie.com>
2020-09-19 11:14:49 -0600

    fixing package install

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='invoicer',
-    packages=['invoicer'],
+    packages=find_packages(),
     include_package_data=True,
 )
